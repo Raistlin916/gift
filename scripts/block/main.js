66a10687e69d4f5d48401476b32349bc0194a1bf
gaokai <gaokai@qima-inc.comgit>
2015-12-24 11:13:07 +0800

    improve

--- scripts/block/main.js
+++ scripts/block/main.js
@@ -2,7 +2,8 @@ import Cycle from '../base/cycle';
 import Input from '../base/input';
 import {GravityBlockCollection} from './gravity_block';
 import {MysteryBlockCollection} from './mystery_block';
-import {imgBase64} from './data';
+import {imgBase64, textData} from './data';
+import {createHeartsByHeart} from '../heart/heart';
 
 const container = document.querySelector('.canvas-container');
 const canvas = document.createElement('canvas');
@@ -17,6 +18,7 @@ cycle.start();
 
 const objs = cycle.getObjs();
 const blocks = new GravityBlockCollection();
+const textBlocks = new MysteryBlockCollection();
 
 
 function createImageDataReader(img) {
@@ -60,6 +62,30 @@ function construct (dx, dy, blocks, img, size) {
 }
 const img = new Image();
 img.onload = function () {
-    construct(100, 50, blocks, img, 20);
+    construct(300, 50, blocks, img, 20);
 }
 img.src = imgBase64;
+
+function constructText (dx, dy, blocks, data) {
+    blocks.reset();
+    for (let i=0; i<data.h; i++) {
+        for (let j=0; j<data.w; j++) {
+            let index = i * data.w + j;
+            if (data.list.indexOf(index) == -1) {
+                continue;
+            }
+            blocks.add({
+                x: dx + j * 12,
+                y: dy + i * 12,
+                w: 10,
+                h: 10,
+                color: 'rgb(255, 160, 32)'
+            });
+        }
+    }
+    objs.add(blocks);
+}
+
+constructText(0, 150, textBlocks, textData);
+
+canvas.onclick = e => createHeartsByHeart(cycle.getObjs(), e.offsetX, e.offsetY);
